b2d963dae206399a900070e1d3d2928a6b4d48fd
Lars Eckart <lars.eckart@hey.com>
2023-12-06 13:36:21 +0200

    wip

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,12 +1,18 @@
 package org.leanpoker.player;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    private static final Logger log = getLogger(Player.class);
+
+    static final String VERSION = "log request";
 
     public static int betRequest(JsonNode request) {
+        log.info("Request: {}", request);
         return 0;
     }
 
