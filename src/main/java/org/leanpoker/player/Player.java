64b43d7dc5d07d82570ef4676b91fa05cda96b34
Lars Eckart <lars.eckart@hey.com>
2023-12-06 14:15:00 +0200

    wip

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,6 +2,7 @@ package org.leanpoker.player;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
+import org.slf4j.MDC;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -9,11 +10,14 @@ public class Player {
 
     private static final Logger log = getLogger(Player.class);
 
-    static final String VERSION = "diff log config";
+    static final String VERSION = "extract game id";
 
     public static int betRequest(JsonNode request) {
         log.info("hello world");
         log.info("Request: {}", request);
+        // get game_id
+        String gameId = request.get("game_id").asText();
+        MDC.put("game_id", gameId);
         return 0;
     }
 
